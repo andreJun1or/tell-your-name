b68130536967a9e7ead895bba6ae0a00ef47f3fd
andreJun1or <andregamer635@gmail.com>
2023-12-11 06:16:07 -0300

    chore: input criado
    falta criar o botão e a lógica para exibir o nome na tela

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text style={styles.title}>Tell your name</Text>
+      <Text style={styles.textSize}>Qual seu nome?</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Nome'
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +22,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 36,
+    marginBottom: 5
+  },
+  textSize: {
+    fontSize: 16
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 273,
+    textAlign: 'center'
+  }
 });
