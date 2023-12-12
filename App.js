bd13b5bba82deca3727a4987e5dfbafc49e5372a
andreJun1or <andregamer635@gmail.com>
2023-12-11 22:26:21 -0300

    feat: não sei o que escrever

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [name, setName] = useState('');
+
+  const showName = () => {
+    Alert.alert(`Olá ${name}`);
+    setName('');
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tell your name</Text>
       <Text style={styles.textSize}>Qual seu nome?</Text>
       <TextInput
+        onChangeText={text => setName(text)}
+        value={name}
         style={styles.input}
         placeholder='Nome'
+      />
+      <Button
+        onPress={showName}
+        title='Cuida'
+        color='#841584'
       />
       <StatusBar style="auto" />
     </View>
